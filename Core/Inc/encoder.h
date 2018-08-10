15adf69829661f24fd0944aba3c65424b608ef04
STRV <strv.analoger@gmail.com>
2018-08-10 10:43:24 +0900

    エンコーダが読めるようになった

--- Core/Inc/encoder.h
+++ Core/Inc/encoder.h
@@ -8,6 +8,11 @@
 #ifndef INC_ENCODER_H_
 #define INC_ENCODER_H_
 
+#define ENC_TIM TIM3
 
+#include "main.h"
+
+void enc_init(void);
+uint32_t enc_get(void);
 
 #endif /* INC_ENCODER_H_ */
--- Core/Src/encoder.c
+++ Core/Src/encoder.c
@@ -7,3 +7,12 @@
 
 #include "encoder.h"
 
+void enc_init(void){
+	LL_TIM_CC_EnableChannel(ENC_TIM, LL_TIM_CHANNEL_CH1);
+	LL_TIM_CC_EnableChannel(ENC_TIM, LL_TIM_CHANNEL_CH2);
+	LL_TIM_EnableCounter(ENC_TIM);
+}
+
+uint32_t enc_get(void){
+	LL_TIM_GetCounter(ENC_TIM);
+}
